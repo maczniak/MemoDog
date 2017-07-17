65f8490df763120a3c6ee99adf9fbf113b8d0a08
전정호 <jeongho@jeonjeoghouimbp.lan>
2017-07-18 01:28:46 +0900

    empty project

---
+++ MemoDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoDog
+//
+//  Created by 전정호 on 2017. 7. 18..
+//  Copyright © 2017년 DoDoable iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MemoDog
+
+basic iOS memo app
+
+## TODO
+
+* save
+* document based
+* keyboard control
+* multiple memos
+* package manager
+* icon
+
